7b0bf0c13800dbaaccf28abe0bbb13eb077c4f09
yuvipanda <yuvipanda@gmail.com>
2018-03-01 12:01:39 -0800

    Add LTI Authentication
    - Shows authenticated users their uid, shows only content to
      others.
    - Copied LTILaunchValidator from LTIAuthenticator

---
+++ homeworkupload/app.py
@@ -0,0 +1,102 @@
+import os
+import argparse
+import sys
+import yaml
+from jinja2 import Environment, FileSystemLoader
+from CommonMark import commonmark
+from tornado import httpserver, ioloop, web
+from homeworkupload.validator import LTILaunchValidator, LTILaunchValidationError
+
+
+class HomeWorkHandler(web.RequestHandler):
+    def render_template(self, name, **extra_ns):
+        """Render an HTML page"""
+        ns = {
+            'static_url': self.static_url,
+            'commonmark': commonmark
+        }
+        ns.update(extra_ns)
+        template = self.settings['jinja2_env'].get_template(name)
+        html = template.render(**ns)
+        self.write(html)
+
+    def get(self, hw):
+        homework_definitions = self.settings['homework_definitions']
+        if hw not in homework_definitions:
+            raise web.HTTPError(404)
+
+        user_id = self.get_secure_cookie('user_id')
+
+        homework = homework_definitions[hw]
+        self.render_template('main.html', homework=homework, user_id=user_id)
+
+    def post(self, hw):
+        # FIXME: Run a process that cleans up old nonces every other minute
+        consumers = self.settings['consumers']
+        validator = LTILaunchValidator(consumers)
+
+        args = {}
+        for k, values in self.request.body_arguments.items():
+            # Convert everything to strings rather than bytes
+            args[k] = values[0].decode() if len(values) == 1 else [v.decode() for v in values]
+
+
+        try:
+            if validator.validate_launch_request(
+                    self.request.full_url(),
+                    self.request.headers,
+                    args
+            ):
+                self.set_secure_cookie(
+                    'user_id', self.get_body_argument('user_id'), expires_days=3, path=self.request.path
+                )
+                self.redirect(self.request.full_url())
+        except LTILaunchValidationError as e:
+            raise web.HTTPError(401, e.message)
+        pass
+
+def main():
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument(
+        'homework_definitions',
+        help='Path to YAML file containing homework definitions'
+    )
+
+    args = argparser.parse_args()
+
+    if 'COOKIE_SECRET' not in os.environ:
+        print('Set a 32byte hex-encoded value as COOKIE_SECRET environment variable first!')
+        sys.exit(1)
+
+    if 'LTI_PASSPORT' not in os.environ:
+        print('Set an EdX formatted LTI Passport as LTI_PASSPORT environment variable first!')
+        sys.exit(1)
+
+    passport_split = os.environ['LTI_PASSPORT'].split(':')
+    consumers = {passport_split[1]: passport_split[2]}
+    with open(args.homework_definitions) as f:
+        homework_definitions = yaml.safe_load(f)
+
+    jinja2_env = Environment(loader=FileSystemLoader([
+        os.path.join(os.path.dirname(__file__), 'templates')
+    ]), autoescape=True)
+
+    settings = {
+        'jinja2_env': jinja2_env,
+        'homework_definitions': homework_definitions,
+        'static_path': os.path.join(os.path.dirname(__file__), "static"),
+        'cookie_secret': os.environ['COOKIE_SECRET'],
+        'consumers': consumers
+    }
+
+    application = web.Application([
+        (r"/(\w+)", HomeWorkHandler),
+    ], **settings)
+
+    http_server = httpserver.HTTPServer(application)
+    http_server.listen(9595)
+    ioloop.IOLoop.current().start()
+
+
+if __name__ == "__main__":
+    main()
